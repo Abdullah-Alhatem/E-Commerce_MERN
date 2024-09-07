13d42c3f02b6ca5a338b21117d7d57b55dcc621a
Abdullah Al-hatem <abdhatm42@gmail.com>
2024-09-07 15:53:36 +0300

    Product feature

---
+++ src/services/productService.ts
@@ -0,0 +1,17 @@
+import productModel from "../models/productModel";
+
+export const getAllProducts = async () => {
+  return await productModel.find();
+};
+
+export const seedInitialProducts = async () => {
+  const products = [
+    { title: "Dell laptop", image: "https://images.esellerpro.com/2660/I/328/155/E203NA-FD026TS_2._1..jpg", price: 15000, stock: 10 },
+  ];
+
+  const existingProducts = await getAllProducts();
+
+  if (existingProducts.length === 0) {
+    await productModel.insertMany(products);
+  }
+};
