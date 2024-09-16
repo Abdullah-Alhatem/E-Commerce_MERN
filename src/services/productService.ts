0c1735d0909397d246b3a2f091c82ff081a16a4d
Abdullah Al-hatem <abdhatm42@gmail.com>
2024-09-16 17:05:17 +0300

    Data Validation with Try & Catch

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -5,13 +5,21 @@ export const getAllProducts = async () => {
 };
 
 export const seedInitialProducts = async () => {
-  const products = [
-    { title: "Dell laptop", image: "https://images.esellerpro.com/2660/I/328/155/E203NA-FD026TS_2._1..jpg", price: 15000, stock: 10 },
-  ];
-
-  const existingProducts = await getAllProducts();
-
-  if (existingProducts.length === 0) {
-    await productModel.insertMany(products);
+  try {
+    const products = [
+      {
+        title: "Dell laptop",
+        image:
+          "https://images.esellerpro.com/2660/I/328/155/E203NA-FD026TS_2._1..jpg",
+        price: 15000,
+        stock: 10,
+      },
+    ];
+    const existingProducts = await getAllProducts();
+    if (existingProducts.length === 0) {
+      await productModel.insertMany(products);
+    }
+  } catch (err) {
+    console.error("connot see database", err);
   }
 };
