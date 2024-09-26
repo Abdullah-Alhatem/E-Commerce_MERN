2e29ab0017931f1b9fe8591de864bd3ade973c74
Abdullah Al-hatem <abdhatm42@gmail.com>
2024-09-26 15:57:40 +0300

    All Products in Home Page + Integration with API

--- backend/src/services/productService.ts
+++ backend/src/services/productService.ts
@@ -10,9 +10,23 @@ export const seedInitialProducts = async () => {
       {
         title: "Dell laptop",
         image:
-          "https://images.esellerpro.com/2660/I/328/155/E203NA-FD026TS_2._1..jpg",
+          "https://th.bing.com/th/id/R.1429d80f6e5f62fadfc6646e43689e5f?rik=ojmUoMmlb32Tog&riu=http%3a%2f%2fwww.informationq.com%2fwp-content%2fuploads%2f2013%2f11%2fDell-Inspiron-15-3521-15.6-inch-Laptop-Black01.jpg&ehk=pSUmBqiCYQyti2AeZ0mRSr1R6M41QW7qxUuorQ3kEbs%3d&risl=&pid=ImgRaw&r=0",
         price: 15000,
         stock: 10,
+      },
+      {
+        title: "Asus laptop",
+        image:
+          "https://th.bing.com/th/id/R.07e8130e38146d0a50646a68b691d4eb?rik=QDxJ2cQfafDM4g&pid=ImgRaw&r=0",
+        price: 18000,
+        stock: 7,
+      },
+      {
+        title: "HP laptop",
+        image:
+          "https://www.hp.com/ca-en/shop/html/Merch/Images/c05971638_1750x1285.jpg",
+        price: 11000,
+        stock: 18,
       },
     ];
     const existingProducts = await getAllProducts();
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,42 @@
-
+import Grid from "@mui/material/Grid2";
+import ProductCard from "../components/ProductCard";
+import { useEffect, useState } from "react";
+import { Product } from "../types/product";
+import { BASE_URL } from "../constants/baseUrl";
+import { Box } from "@mui/material";
 
 const HomePage = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}/product`);
+        const data = await response.json();
+        setProducts(data);
+      } catch {
+        setError(true);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <Box>Something went wrong , please try again!</Box>;
+  }
+
   return (
-    <div>Home page</div>
-  )
-}
+    <>
+      <Grid container spacing={2} sx={{ mt: 2, ml: 4, mr: 4 }}>
+        {products.map(p => (
+          <Grid size={{ md: 4 }}>
+            <ProductCard {...p} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
+};
 
 export default HomePage;
